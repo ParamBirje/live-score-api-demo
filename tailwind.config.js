aebed996efed3ae4031214fbdc877eb2cfbd9c4a
Param Birje <parambirje@gmail.com>
2024-03-19 17:45:45 +0530

    Changed font to Montserrat

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,12 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        monty: ["Montserrat", "serif"]
+      },
+
+    },
   },
   plugins: [],
 }
